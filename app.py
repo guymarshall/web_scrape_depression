550f2d941b71f0291b5b574dec9ada27d889660d
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-10-04 12:14:15 +0100

    Add comment for finding false positives before progressing onto loops

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ words_to_search_for = [
     "fine", "okay", "help", "down", "tired", "motivation", "lack", "alone", "friend", "pain", "hurt", "self", "harm", "want", "here"
 ]
 
+# find false positives
 
 # for loop instagram
 
