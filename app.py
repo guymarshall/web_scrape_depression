ec00268eebfd389553aae63e56bb0bf93e24f4bd
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-10-04 12:21:45 +0100

    Add remove_false_positives function definition - need to add logic

--- app.py
+++ app.py
@@ -3,7 +3,15 @@ words_to_search_for = [
     "fine", "okay", "help", "down", "tired", "motivation", "lack", "alone", "friend", "pain", "hurt", "self", "harm", "want", "here"
 ]
 
-# find false positives
+# find and remove false positives
+def remove_false_positives(word_list):
+    corrected_list = []
+    for word in word_list:
+        corrected_list.append(word)
+    
+    # do logic for removing false positives
+
+    return corrected_list
 
 # for loop instagram
 
