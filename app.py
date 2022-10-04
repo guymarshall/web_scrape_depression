4241dd56e0c9145f3c8bfc830c2039c2eb022924
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-10-04 12:22:22 +0100

    Remove false positives - makes it too complicated for now

--- app.py
+++ app.py
@@ -3,16 +3,6 @@ words_to_search_for = [
     "fine", "okay", "help", "down", "tired", "motivation", "lack", "alone", "friend", "pain", "hurt", "self", "harm", "want", "here"
 ]
 
-# find and remove false positives
-def remove_false_positives(word_list):
-    corrected_list = []
-    for word in word_list:
-        corrected_list.append(word)
-    
-    # do logic for removing false positives
-
-    return corrected_list
-
 # for loop instagram
 
 # for loop twitter
